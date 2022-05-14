4aa4c607a28fc263edc5d14f08531471422f569a
Jasser Alshaer <joshaer17@gmail.com>
2022-05-14 04:38:24 +0300

    com

--- Makaani/Controllers/HomeController.cs
+++ Makaani/Controllers/HomeController.cs
@@ -24,6 +24,12 @@ namespace Makaani.Controllers
         {
             return View();
         }
+
+        public IActionResult SearchForEstate(string keyWord,int categotyId,int typeId)
+        {
+            return View();
+        }
+
         public IActionResult AboutUs()
         {
             return View();
@@ -63,10 +69,64 @@ namespace Makaani.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Login(String accountUser, String Password)
+        {
+            
+            if (accountUser.Contains("@"))
+            {
+                var login = _context.Login.Where(x => x.Email == accountUser && x.Password == Password).Single();
+                if (login == null)
+                {
+                    return Unauthorized();
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Saler");
+                }
+            }
+            else
+            {
+                var login = _context.Login.Where(x => x.Phone == accountUser && x.Password == Password).Single();
+                if (login == null)
+                {
+                    return Unauthorized();
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Saler");
+                }
+            }
+            
+        }
 
         public IActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Register(string FullName, string Address, string Nationality, DateTime? BirthDate,String Email,String Password,String Phone)
+        {
+            User makamiUser=new User();
+            makamiUser.FullName = FullName;
+            makamiUser.Address = Address;
+            makamiUser.Nationality = Nationality;
+            makamiUser.BirthDate = BirthDate;
+
+            _context.Add(makamiUser);
+            _context.SaveChanges();
+
+            Login userCredintials=new Login();
+            userCredintials.Email = Email;
+            userCredintials.Password = Password;
+            userCredintials.Phone = Phone;
+            userCredintials.RoleId = 1;
+            userCredintials.UserId= _context.User.OrderByDescending(x => x.UserId).First().UserId;
+
+            _context.Add(userCredintials);
+            _context.SaveChanges();
+
+            return RedirectToAction("Login");
         }
 
     }
